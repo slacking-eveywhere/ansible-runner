f7dee57c8883d8aedd2c8daa8c731e8d04cb3fda
Luc Deplanche <luc.deplanche@cs-soprasteria.com>
2025-03-20 16:21:01 +0100

    Removing wget reference
    wget is no longer necessary
    so testing if it is installed is not a good idea

--- install.sh
+++ install.sh
@@ -4,12 +4,6 @@ INSTALL_DIR=${HOME}/.local/bin
 SCRIPT_NAME=ansible-runner
 ANSIBLE_RUNNER_SCRIPT_URL=https://raw.githubusercontent.com/slacking-eveywhere/ansible-runner/refs/heads/main/ansible-runner
 
-
-if ! command -v wget >/dev/null 2>&1; then
-    echo "wget is NOT installed. Please install it."
-    exit 1
-fi
-
 mkdir -p "${INSTALL_DIR}"
 curl -s -o "${INSTALL_DIR}/${SCRIPT_NAME}" "${ANSIBLE_RUNNER_SCRIPT_URL}"
 chmod u+x "${INSTALL_DIR}/${SCRIPT_NAME}"
