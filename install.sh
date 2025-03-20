381d83053aba25ba5b8382abf25c481da566f0dc
Luc Deplanche <luc.deplanche@cs-soprasteria.com>
2025-03-20 16:20:23 +0100

    Add a little text when install is done
    because feedback is nice

--- install.sh
+++ install.sh
@@ -14,6 +14,12 @@ mkdir -p "${INSTALL_DIR}"
 curl -s -o "${INSTALL_DIR}/${SCRIPT_NAME}" "${ANSIBLE_RUNNER_SCRIPT_URL}"
 chmod u+x "${INSTALL_DIR}/${SCRIPT_NAME}"
 
+echo "Installation done."
+echo ""
+echo "Using :"
+echo "ansible-runner --help"
+echo "to see how it works."
+
 if [[ ":$PATH:" != *":${HOME}/.local/bin:"* ]]; then
     echo "Adding ~/.local/bin to PATH"
     export PATH="${HOME}/.local/bin:${PATH}"
